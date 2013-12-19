3182742b8bcd63b009cf801c815a465df058e359
Connor Peet <connor@connorpeet.com>
2013-12-19 17:50:44 -0600

    Minecraft plans

---
+++ dist/config.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+	'plans' => array(
+		array('name' => 'Mercury', 'size' => '256MB', 'price' => '1.99'),
+		array('name' => 'Venus', 'size' => '512MB', 'price' => '3.99'),
+		array('name' => 'Earth', 'size' => '765MB', 'price' => '5.99'),
+		array('name' => 'Mars', 'size' => '1GB', 'price' => '7.99'),
+		array('name' => 'Jupiter', 'size' => '1.5GB', 'price' => '11.99'),
+		array('name' => 'Saturn', 'size' => '2.0GB', 'price' => '15.99'),
+		array('name' => 'Uranus', 'size' => '2.5GB', 'price' => '19.99'),
+		array('name' => 'Neptune', 'size' => '3.0GB', 'price' => '23.99'),
+		array('name' => 'Lunar', 'size' => '3.5GB', 'price' => '27.99'),
+		array('name' => 'Nebula', 'size' => '4.0GB', 'price' => '31.99'),
+		array('name' => 'Nova', 'size' => '4.5GB', 'price' => '34.99'),
+		array('name' => 'Supernova', 'size' => '5.5GB', 'price' => '44.99'),
+		array('name' => 'Galactic', 'size' => '6.0GB', 'price' => '47.99')
+	)
+);
