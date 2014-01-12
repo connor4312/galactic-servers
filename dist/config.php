615a641bc73b1a2e04bdb73d3f95e6dac7315f6e
Connor Peet <connor@connorpeet.com>
2014-01-11 20:48:40 -0600

    New plans and fixes

--- dist/config.php
+++ dist/config.php
@@ -2,20 +2,20 @@
 
 return array(
 	'plans' => array(
-		array('name' => 'Mercury', 'size' => '256MB', 'price' => '1.99'),
-		array('name' => 'Venus', 'size' => '512MB', 'price' => '3.99'),
-		array('name' => 'Earth', 'size' => '765MB', 'price' => '5.99'),
-		array('name' => 'Mars', 'size' => '1GB', 'price' => '7.99'),
-		array('name' => 'Jupiter', 'size' => '1.5GB', 'price' => '11.99'),
-		array('name' => 'Saturn', 'size' => '2.0GB', 'price' => '15.99'),
-		array('name' => 'Uranus', 'size' => '2.5GB', 'price' => '19.99'),
-		array('name' => 'Neptune', 'size' => '3.0GB', 'price' => '23.99'),
-		array('name' => 'Lunar', 'size' => '3.5GB', 'price' => '27.99'),
-		array('name' => 'Nebula', 'size' => '4.0GB', 'price' => '31.99'),
-		array('name' => 'Nova', 'size' => '4.5GB', 'price' => '34.99'),
-		array('name' => 'Supernova', 'size' => '5.0GB', 'price' => '39.99'),
-		array('name' => 'Galactic', 'size' => '5.5GB', 'price' => '44.99'),
-		array('name' => 'Universal', 'size' => '6.0GB', 'price' => '47.99')
+		array('name' => 'Mercury', 'size' => '256MB', 'price' => '1.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=5'),
+		array('name' => 'Venus', 'size' => '512MB', 'price' => '3.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=4'),
+		array('name' => 'Earth', 'size' => '765MB', 'price' => '5.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=6'),
+		array('name' => 'Mars', 'size' => '1GB', 'price' => '7.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=2'),
+		array('name' => 'Jupiter', 'size' => '1.5GB', 'price' => '11.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=7'),
+		array('name' => 'Saturn', 'size' => '2.0GB', 'price' => '15.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=8'),
+		array('name' => 'Uranus', 'size' => '2.5GB', 'price' => '19.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=9'),
+		array('name' => 'Neptune', 'size' => '3.0GB', 'price' => '23.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=10'),
+		array('name' => 'Lunar', 'size' => '3.5GB', 'price' => '27.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=11'),
+		array('name' => 'Nebula', 'size' => '4.0GB', 'price' => '31.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=12'),
+		array('name' => 'Nova', 'size' => '4.5GB', 'price' => '34.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=3'),
+		array('name' => 'Supernova', 'size' => '5.0GB', 'price' => '39.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=13'),
+		array('name' => 'Galactic', 'size' => '5.5GB', 'price' => '44.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=14'),
+		array('name' => 'Universal', 'size' => '6.0GB', 'price' => '47.99', 'url' => 'https://my.galacticservers.com/cart.php?a=add&pid=15')
 	),
 	'dedicated' => array(
 		array(
